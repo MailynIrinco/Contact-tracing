e69ff4bd5e9eb41640b1e8197773085d9ecfbd48
MailynIrinco <irincomailyn@gmail.com>
2022-06-08 19:31:50 +0800

    Fill in text boxes

--- Contact tracing/Form1.Designer.cs
+++ Contact tracing/Form1.Designer.cs
@@ -28,12 +28,241 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.box1 = new System.Windows.Forms.TextBox();
+            this.box2 = new System.Windows.Forms.TextBox();
+            this.box3 = new System.Windows.Forms.TextBox();
+            this.box4 = new System.Windows.Forms.TextBox();
+            this.box5 = new System.Windows.Forms.TextBox();
+            this.box6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.savebutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Address:";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(12, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(216, 25);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Phone No. / Telephone No.";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(368, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 25);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Age:";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(368, 146);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 25);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Date:";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(368, 213);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 25);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Time in/out:";
+            // 
+            // box1
+            // 
+            this.box1.Location = new System.Drawing.Point(12, 116);
+            this.box1.Name = "box1";
+            this.box1.Size = new System.Drawing.Size(350, 27);
+            this.box1.TabIndex = 13;
+            // 
+            // box2
+            // 
+            this.box2.Location = new System.Drawing.Point(12, 174);
+            this.box2.Name = "box2";
+            this.box2.Size = new System.Drawing.Size(350, 27);
+            this.box2.TabIndex = 14;
+            // 
+            // box3
+            // 
+            this.box3.Location = new System.Drawing.Point(12, 241);
+            this.box3.Name = "box3";
+            this.box3.Size = new System.Drawing.Size(350, 27);
+            this.box3.TabIndex = 15;
+            // 
+            // box4
+            // 
+            this.box4.Location = new System.Drawing.Point(368, 116);
+            this.box4.Name = "box4";
+            this.box4.Size = new System.Drawing.Size(125, 27);
+            this.box4.TabIndex = 16;
+            // 
+            // box5
+            // 
+            this.box5.Location = new System.Drawing.Point(368, 174);
+            this.box5.Name = "box5";
+            this.box5.Size = new System.Drawing.Size(125, 27);
+            this.box5.TabIndex = 17;
+            // 
+            // box6
+            // 
+            this.box6.Location = new System.Drawing.Point(370, 241);
+            this.box6.Name = "box6";
+            this.box6.Size = new System.Drawing.Size(125, 27);
+            this.box6.TabIndex = 18;
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(45, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(417, 57);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Contact tracing form are for safety purposes only, we take your informations conf" +
+    "idential.";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Location = new System.Drawing.Point(12, 286);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(350, 30);
+            this.checkBox1.TabIndex = 20;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.Location = new System.Drawing.Point(12, 318);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(350, 30);
+            this.checkBox2.TabIndex = 21;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.Location = new System.Drawing.Point(12, 354);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(350, 30);
+            this.checkBox3.TabIndex = 22;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.Location = new System.Drawing.Point(12, 390);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(350, 30);
+            this.checkBox4.TabIndex = 23;
+            this.checkBox4.Text = "checkBox4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.Location = new System.Drawing.Point(12, 426);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(350, 30);
+            this.checkBox5.TabIndex = 24;
+            this.checkBox5.Text = "checkBox5";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // savebutton
+            // 
+            this.savebutton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.savebutton.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.savebutton.Location = new System.Drawing.Point(370, 486);
+            this.savebutton.Name = "savebutton";
+            this.savebutton.Size = new System.Drawing.Size(123, 29);
+            this.savebutton.TabIndex = 25;
+            this.savebutton.Text = "Save";
+            this.savebutton.UseVisualStyleBackColor = false;
+            this.savebutton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(507, 527);
+            this.Controls.Add(this.savebutton);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.box6);
+            this.Controls.Add(this.box5);
+            this.Controls.Add(this.box4);
+            this.Controls.Add(this.box3);
+            this.Controls.Add(this.box2);
+            this.Controls.Add(this.box1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Contact Tracing Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox box1;
+        private TextBox box2;
+        private TextBox box3;
+        private TextBox box4;
+        private TextBox box5;
+        private TextBox box6;
+        private Label label7;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private CheckBox checkBox4;
+        private CheckBox checkBox5;
+        private Button savebutton;
     }
 }
--- Contact tracing/Form1.cs
+++ Contact tracing/Form1.cs
@@ -1,3 +1,5 @@
+using System.IO;
+
 namespace Contact_tracing
 {
     public partial class Form1 : Form
@@ -5,6 +7,18 @@ namespace Contact_tracing
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter file = new StreamWriter(@"C:\Users\irinc\OneDrive\Documents\Contact tracing.text");
+            file.WriteLine("Name:" + box1.Text);
+            file.WriteLine("Address:" + box2.Text);
+            file.WriteLine("Phone No." + box3.Text);
+            file.WriteLine("Age:" + box4.Text);
+            file.WriteLine("Date:" + box5.Text);
+            file.WriteLine("Time in/out:" + box6.Text);
+            file.Close();   
         }
     }
 }
